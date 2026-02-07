5e33a6ceafafbd8a735ce184750100b1b74a9b41
zhmurandrii <249627766+zhmurandrii@users.noreply.github.com>
2026-02-07 13:19:39 +0200

    add buttons for user choice

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,3 +1,18 @@
+const btnRock = document.getElementById("btn-rock");
+const btnPaper = document.getElementById("btn-paper");
+const btnScissors = document.getElementById("btn-scissors");
+const container = document.getElementById("container");
+
+const paraScore = document.createElement("para");
+paraScore.textContent = "0:0";
+const paraRound = document.createElement("p");
+container.appendChild(paraScore);
+container.appendChild(paraRound);
+
+btnRock.addEventListener("click", playRock);
+btnPaper.addEventListener("click", playPaper);
+btnScissors.addEventListener("click", playScissors);
+
 let userChoice;
 let winCountUser = 0;
 let winCountComputer = 0;
@@ -7,20 +22,37 @@ let winningVariant;
 let userVariant;
 
 function gameCheck() {
-  if (gamesCount >= 5) {
+  if (gamesCount === 5) {
     gameStop();
+    return true;
   } else {
-    gameStart();
-  }
+    return false;
+  } /*else {
+    playRound();
+  }*/
+}
+
+function startNewGame() {
+  gamesCount = 0;
+  winCountUser = 0;
+  winCountComputer = 0;
+  container.style.backgroundColor = "white";
+  paraScore.textContent = "0:0";
+  paraRound.textContent = "New game started!";
 }
 
 function gameStop() {
   if (winCountUser > winCountComputer) {
-    alert(`You WON! Final score is ${winCountUser}:${winCountComputer}`);
+    paraRound.textContent = `You WON! Final score is ${winCountUser}:${winCountComputer}`;
+    container.style.backgroundColor = "hsl(120, 60%, 54%)";
+    //alert(`You WON! Final score is ${winCountUser}:${winCountComputer}`);
   } else if (winCountUser < winCountComputer) {
-    alert(`You LOST! Final score is ${winCountUser}:${winCountComputer}`);
+    paraRound.textContent = `You LOST! Final score is ${winCountUser}:${winCountComputer}`;
+    container.style.backgroundColor = "hsl(0, 68%, 49%)";
+    //alert(`You LOST! Final score is ${winCountUser}:${winCountComputer}`);
   } else {
-    alert(`It's a TIE! Final score is ${winCountUser}:${winCountComputer}`);
+    paraRound.textContent = `It's a TIE! Final score is ${winCountUser}:${winCountComputer}`;
+    //alert(`It's a TIE! Final score is ${winCountUser}:${winCountComputer}`);
   }
 }
 
@@ -35,9 +67,11 @@ function getComputerChoice() {
   }
 }
 
-function gameStart() {
-  userVariant = prompt("Enter your choice: ");
-  userChoice = userVariant.toLowerCase();
+function playRound() {
+  if (gamesCount >= 5) {
+    startNewGame();
+    return;
+  }
   if (
     userChoice === "rock" ||
     userChoice === "paper" ||
@@ -49,10 +83,14 @@ function gameStart() {
     console.log(`Computer choice is: ${computerChoice}`);
 
     if (userChoice === computerChoice) {
-      console.log("It's a tie!");
+      //console.log("It's a tie!");
+      paraRound.textContent = "It's a tie!";
       console.log("------------------------");
-      alert(`It's a tie! Score is ${winCountUser}:${winCountComputer}`);
+      paraScore.textContent = `${winCountUser}:${winCountComputer}`;
+      container.style.backgroundColor = "white";
+      //alert(`It's a tie! Score is ${winCountUser}:${winCountComputer}`);
       gamesCount++;
+      console.log(gamesCount);
       gameCheck();
     } else {
       if (
@@ -64,20 +102,28 @@ function gameStart() {
         console.log(`You won!`);
         console.log("------------------------");
         winningVariant = userChoice.toUpperCase();
-        alert(
+        paraRound.textContent = `You won! ${winningVariant} beats ${computerChoice}!`;
+        paraScore.textContent = `${winCountUser}:${winCountComputer}`;
+        container.style.backgroundColor = "hsl(120, 27%, 56%)";
+        /*alert(
           `You won! ${winningVariant} beats ${computerChoice}! Score is ${winCountUser}:${winCountComputer}`,
-        );
+        );*/
         gamesCount++;
+        console.log(gamesCount);
         gameCheck();
       } else {
         winCountComputer++;
         console.log("You lost!");
         console.log("------------------------");
         winningVariant = computerChoice.toUpperCase();
-        alert(
+        paraRound.textContent = `You lost! ${userChoice} loses to ${winningVariant}`;
+        paraScore.textContent = `${winCountUser}:${winCountComputer}`;
+        container.style.backgroundColor = "hsl(0, 57%, 59%)";
+        /*alert(
           `You lost! ${userChoice} loses to ${winningVariant} Score is ${winCountUser}:${winCountComputer}`,
-        );
+        );*/
         gamesCount++;
+        console.log(gamesCount);
         gameCheck();
       }
     }
@@ -88,4 +134,23 @@ function gameStart() {
   }
 }
 
-gameStart();
+function playRock() {
+  userChoice = "rock";
+  playRound();
+}
+function playPaper() {
+  userChoice = "paper";
+  playRound();
+}
+function playScissors() {
+  userChoice = "scissors";
+  playRound();
+}
+
+function gameStart() {
+  userVariant = prompt("Enter your choice: ");
+  userChoice = userVariant.toLowerCase();
+  playRound();
+}
+
+//gameStart();
